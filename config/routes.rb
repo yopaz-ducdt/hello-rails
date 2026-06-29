d3d0d071bedc3f5b719711a0d5bc401d074002a7
yopaz-ducdt <daotrungduc.it@gmail.com>
2026-06-29 14:22:16 +0700

    feat: add users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   get "homepage/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
