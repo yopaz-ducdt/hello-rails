2651f0ea42e25701b45c39de40c0608282719cd4
yopaz-ducdt <daotrungduc.it@gmail.com>
2026-07-03 10:07:21 +0700

    feat: chapter 5 and fix assert_template error

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,30 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @base_title = "Ruby on Rails"
+  end
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
