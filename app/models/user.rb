3351332c7d3ad5dc4a3215c3eadabae5106a7370
yopaz-ducdt <daotrungduc.it@gmail.com>
2026-07-06 16:00:04 +0700

    feat: session manager (#7)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Tạo một biến ảo (tạm thời) để chứa token thô trước khi đưa xuống cookie
+  attr_accessor :remember_token
+
+  before_save { email.downcase! }
+
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,4 +14,31 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Hàm mã hóa chuỗi thô (dùng chung cho cả kiểm tra test sau này)
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Hàm sinh ra một chuỗi ngẫu nhiên an toàn (Token)
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Hàm xử lý ghi nhớ: sinh token thô và lưu bản băm vào DB
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Kiểm tra xem token thô gửi lên có khớp với bản băm trong DB không
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
