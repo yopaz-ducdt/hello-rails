90e5d958691a122865dd1d562acd2d49818cc167
yopaz-ducdt <daotrungduc.it@gmail.com>
2026-07-06 14:21:50 +0700

    feat: log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   root "static_pages#home"
 
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :microposts
   resources :users
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  get "/signup", to: "users#new"
-
   resources :users
 end
